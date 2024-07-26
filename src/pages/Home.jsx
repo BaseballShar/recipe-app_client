c81740eb45bbcbdfb1ebd3a4d81dce17e620bf27
Baseball Shar <workingcth@gmail.com>
2024-07-26 16:03:18 +0800

    Finished fetching recipes and displaying in Home screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    async function fetchRecipes() {
+      try {
+        const res = await axios.get("http://localhost:3001/recipes");
+        setRecipes(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchRecipes();
+  }, []);
+
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt={recipe.name} />
+            <p>Cooking Time: {recipe.cookingTime} mins</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
