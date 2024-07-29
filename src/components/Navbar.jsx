575d10f44a7c8bc1ed6473c35f20454526fa3d62
Baseball Shar <workingcth@gmail.com>
2024-07-29 16:20:34 +0800

    Used a more consistent style for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,14 +13,24 @@ export default function Navbar() {
 
   return (
     <div className="navbar">
-      <Link to={"/"}>Home</Link>
+      <Link className="link" to={"/"}>
+        Home
+      </Link>
       {!cookies.access_token ? (
-        <Link to={"/auth"}>Login / Register</Link>
+        <Link className="link" to={"/auth"}>
+          Login / Register
+        </Link>
       ) : (
         <>
-          <Link to={"/create-recipe"}>Create Recipe</Link>
-          <Link to={"/saved-recipes"}>Saved Recipes</Link>
-          <button onClick={handleLogout}>Logout</button>
+          <Link className="link" to={"/create-recipe"}>
+            Create Recipe
+          </Link>
+          <Link className="link" to={"/saved-recipes"}>
+            Saved Recipes
+          </Link>
+          <span className="link" onClick={handleLogout}>
+            Logout
+          </span>
         </>
       )}
     </div>
